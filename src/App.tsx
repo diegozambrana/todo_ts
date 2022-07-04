aa84a4faa10696c7313a1c0fb3426f51f76ad5a5
Diego Zambrana <contacto@diegozambrana.com>
2022-07-04 19:19:39 -0400

    start

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. Hola mundo este es el curso de React
         </p>
         <a
           className="App-link"
